ad87c1c9650527187d824ca7b1984f26d084c499
Soham Bambade <141113437+soham408@users.noreply.github.com>
2023-11-27 07:01:04 +0000

    variables and datatypes

---
+++ file.js
@@ -0,0 +1,27 @@
+const accountId = "aknaksha408"
+let accountEmail = "slbambade@gmail.com"
+let accountPassword = "40811"
+let accountstate;
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountstate]);
+
+
+
+/* ---------datatypes----------- */
+
+/*
+number
+bighInt
+string = " "
+boolean
+
+null (object)
+*/
+
+/*   how to know type of any value    */
+
+console.log(typeof null);
+
+
+
+console.log(3 +  3);
